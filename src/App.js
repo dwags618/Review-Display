954761ca34a8447b2dee9af469394a8d6921dce4
dwags618 <dwags618@gmail.com>
2018-04-20 05:43:57 -0500

    dropdown menu works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import IndexView from './components/index-view';
+import sortBy from 'sort-by';
 
 class App extends React.Component {
 
@@ -8,8 +9,12 @@ class App extends React.Component {
 
     // set the initial component state
     this.state = {
-      	reviews: ''
-  		}
+      	reviews: '',
+  		value: ''
+  	}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	componentDidMount() {
@@ -18,9 +23,45 @@ class App extends React.Component {
 	                return res.json()
 	             })
 	            .then(data => { 
-	                this.setState({ reviews:data.reviews })
+	                this.setState({ reviews:data.reviews.sort(sortBy('-details.created')) })
 	             });
 	}
+
+	handleChange(event) {
+
+	    this.setState({value: event.target.value});
+
+  	}
+
+  	handleSubmit(event) {
+
+	    event.preventDefault();
+
+	    if(this.state.value==='most_helpful')
+	    {
+	    	this.setState({ reviews:this.state.reviews.sort(sortBy('-metrics.helpful_score')) })
+	    }
+
+	    else if(this.state.value==='most_recent')
+	    {
+	    	this.setState({ reviews:this.state.reviews.sort(sortBy('-details.created')) })
+	    }
+
+	    else if(this.state.value==='lowest_rated')
+	    {
+	    	this.setState({ reviews:this.state.reviews.sort(sortBy('metrics.rating')) })
+	    }
+
+	    else if(this.state.value==='highest_rated')
+	    {
+	    	this.setState({ reviews:this.state.reviews.sort(sortBy('-metrics.rating')) })
+	    }
+
+	    else if(this.state.value==='oldest')
+	    {
+	    	this.setState({ reviews:this.state.reviews.sort(sortBy('details.created')) })
+	    }
+  	}
 
   	render() {
 	    return (
@@ -29,6 +70,18 @@ class App extends React.Component {
 				<div className="App-header">
 					<h2>Review Display</h2>
 				</div>
+				<div style={{marginLeft:40}}>
+					<form onSubmit={this.handleSubmit}>
+			          	<select value={this.state.value} onChange={this.handleChange}>
+				            <option value="most_recent">Most Recent</option>
+				            <option value="most_helpful">Most Helpful</option>
+				            <option value="lowest_rated">Lowest Rated</option>
+				            <option value="highest_rated">Highest Rated</option>
+				            <option value="oldest">Oldest</option>
+			          	</select>
+			          	<input type="submit" value="Submit" />
+	      			</form>
+      			</div>
 				<div id="root"></div>
 				<IndexView
 					reviewBank={this.state.reviews}
